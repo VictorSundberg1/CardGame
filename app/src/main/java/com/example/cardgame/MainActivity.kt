624689766689848a83c4893b3f2c5ff63662edf1
VictorSundberg1 <victor.sundberg1@hotmail.com>
2024-11-15 11:40:41 +0100

    initial commit

---
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.cardgame
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cardgame.databinding.ActivityGameBinding
+import com.example.cardgame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.menuRulesBtn.setOnClickListener{
+            val intent = Intent(this, RulesActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.menuStartBtn.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
